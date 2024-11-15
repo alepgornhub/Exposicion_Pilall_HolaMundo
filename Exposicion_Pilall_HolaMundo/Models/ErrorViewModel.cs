ae4a6aaf2df9d7129e328c28e0e89b97f5abe0b2
avend <avend@ALEJANDRO17>
2024-11-15 16:32:19 -0600

    Agregar archivos de proyecto.

---
+++ Exposicion_Pilall_HolaMundo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Exposicion_Pilall_HolaMundo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
